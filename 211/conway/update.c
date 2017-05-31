197f9c7a73f47441c83ea985b98e0e78eeddf5d0
David Awad <davidawad64@gmail.com>
2017-05-31 11:21:17 -0400

    MAINT: consolidating files and folders

---
+++ 211/conway/update.c
@@ -0,0 +1,27 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+int rule1(){ /*any live cell with <  2 neighbors dies*/ 
+
+} 
+int rule2(){ /*any live cells with 2 or 3 lives */ 
+
+} 
+int rule3(){ /*live cell with >3 neighbors dies */ 
+
+}
+int rule4(){ /*dead cell with three neighbors lives*/ 
+
+}
+int update(int i,int j,**board){
+	int i, j ; 
+	for(i = 0,i<height ,i++){
+		for(j=0,j<width,j++){
+		
+						}
+	}
+
+
+} 
+
+
